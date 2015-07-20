a52f8fb22f76568b02e600bb994f670cd9ceb0c2
hnguyen <hnguyen@exacttarget.com>
2015-07-20 12:51:03 -0400

    basic skeleton for node app done

---
+++ app.js
@@ -0,0 +1,7 @@
+var express     = require('express');
+var app         = express();
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
+app.listen(app.get('port'), function () {
+    console.log('Express server running on port: ' + app.get('port'));    
+});
