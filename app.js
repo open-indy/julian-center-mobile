d44cd997a2a5fae8144ec38ed4bebe465f641f60
jarmstrong <jarmstrong@exacttarget.com>
2015-07-20 15:28:22 -0400

    Basic rest client

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 var express     = require('express');
 var app         = express();
+
+var marketingCloudIntegration = require('./marketing-cloud-integration');
+
+
 app.set('port', process.env.PORT || 3000);
+
 app.use(express.static(__dirname + '/public'));
+
 app.listen(app.get('port'), function () {
     console.log('Express server running on port: ' + app.get('port'));    
 });
+
+marketingCloudIntegration.fireSignupEmail('josh.armstrong@salesforce.com');
---
+++ marketing-cloud-integration.js
@@ -0,0 +1,31 @@
+var RestClient = require('node-rest-client').Client;
+var clientId = process.env.clientId || 'SET clientId Environment Var';
+var clientSecret = process.env.clientSecret || 'SET clientId Environment Var';
+
+client = new RestClient();
+// registering remote methods 
+client.registerMethod("jsonMethod", "http://google.com/", "GET");
+client.registerMethod('authenticate', 'https://auth.exacttargetapis.com/v1/requestToken', 'POST');
+ 
+exports.fireSignupEmail = function(emailAddress) {
+    console.log('Firing email to ' + emailAddress);
+
+    var authArgs = {
+        data: { clientId: 'wf7yq9cdxknsueeescm87mkj', clientSecret: '7My96mxqYF9MZrnDCcx9ZdvR' },
+        headers: { 'Content-Type': 'application/json' }
+    }
+
+    client.methods.authenticate(authArgs, function(data, response) {
+        console.log(data);
+        console.log(response);
+    });
+     
+    /*
+    client.methods.jsonMethod(function(data,response){
+        // parsed response body as js object 
+        console.log(data);
+        // raw response 
+        console.log(response);
+    });
+    */
+};
